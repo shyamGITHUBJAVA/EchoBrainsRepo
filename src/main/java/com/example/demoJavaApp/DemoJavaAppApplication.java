40fdf409650e22214bbf720418921648863866a6
shyam kumar <38135327+shyamGITHUBJAVA@users.noreply.github.com>
2025-05-08 11:29:46 +0530

    initial commit

---
+++ src/main/java/com/example/demoJavaApp/DemoJavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoJavaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoJavaApp/DemoJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoJavaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
